1f861a14e78dedc950bd866a3a4181bea12118bf
JesusBoi007 <81155821+JesusBoi007@users.noreply.github.com>
2023-09-09 15:41:12 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import datetime
+import pytz
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['GET'])
+def get_info():
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    current_day = datetime.datetime.now(pytz.utc).strftime('%A')
+    current_time_utc = datetime.datetime.now(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    github_url_file = " "
+    github_repo_url = "https://github.com/JesusBoi007/Zuri-Internship"
+
+    response_data = {
+        'slack_name': slack_name,
+        'current_day': current_day,
+        'current_utc_time': current_time_utc,
+        'track': track,
+        'github_url_file': github_url_file,
+        'github_repo_url': github_repo_url,
+        'status_code': 200
+    }
+
+    return jsonify(response_data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
